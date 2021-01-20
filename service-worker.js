5f95f5eafde731bb43da42d700c3c312c7587b0e
vovador <vovadorko@gmail.com>
2021-01-20 14:28:58 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.2088c32903613710f8f9678a01b40cd4.js"
+  "./precache-manifest.e5d4805e46ce82a40557ff56ca7bed9b.js"
 );
 
 self.addEventListener('message', (event) => {
